a25c1a434132df023e8441d9aac34771d937be98
Suresh Kumar <skamrani2002@gmail.com>
2018-02-22 17:03:22 +0500

    Add files via upload
    Digit recognization

---
+++ main.py
@@ -0,0 +1,179 @@
+
+# coding: utf-8
+
+# In[28]:
+
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+from collections import Counter
+
+from matplotlib import style
+style.use("ggplot")
+
+def createExamples():
+    numberArrayExamples = open('numArEx.txt','a')
+    numbersWeHave = range(1,10)
+    for eachNum in numbersWeHave:
+        for furtherNum in numbersWeHave:
+
+            imgFilePath = 'images/numbers/'+str(eachNum)+'.'+str(furtherNum)+'.png'
+            ei = Image.open(imgFilePath)
+            eiar = np.array(ei)
+            eiarl = str(eiar.tolist())
+
+            lineToWrite = str(eachNum)+'::'+eiarl+'\n'
+            numberArrayExamples.write(lineToWrite)
+
+
+
+            
+            
+def threshold(imageArray):
+    balanceAr = []
+    newAr = imageArray
+    for eachPart in imageArray:
+        for theParts in eachPart:
+			# for the reduce(lambda x, y: x + y, theParts[:3]) / len(theParts[:3])
+			# in Python 3, just use: from statistics import mean
+			# then do avgNum = mean(theParts[:3])
+            avgNum = reduce(lambda x, y: x + y, theParts[:3]) / len(theParts[:3])
+            balanceAr.append(avgNum)
+    balance = reduce(lambda x, y: x + y, balanceAr) / len(balanceAr)
+    for eachRow in newAr:
+        for eachPix in eachRow:
+            if reduce(lambda x, y: x + y, eachPix[:3]) / len(eachPix[:3]) > balance:
+                eachPix[0] = 255
+                eachPix[1] = 255
+                eachPix[2] = 255
+                eachPix[3] = 255
+            else:
+                eachPix[0] = 0
+                eachPix[1] = 0
+                eachPix[2] = 0
+                eachPix[3] = 255
+    return newAr
+
+
+
+def whatNumIsThis(filePath):
+
+    matchedAr = []
+    loadExamps = open('numArEx.txt','r').read()
+    loadExamps = loadExamps.split('\n')
+    i = Image.open(filePath)
+    iar = np.array(i)
+    iarl = iar.tolist()
+    inQuestion = str(iarl)
+    for eachExample in loadExamps:
+        try:
+            splitEx = eachExample.split('::')
+            currentNum = splitEx[0]
+            currentAr = splitEx[1]
+            eachPixEx = currentAr.split('],')
+            eachPixInQ = inQuestion.split('],')
+            x = 0
+            while x < len(eachPixEx):
+                if eachPixEx[x] == eachPixInQ[x]:
+                    matchedAr.append(int(currentNum))
+
+                x+=1
+        except Exception as e:
+            print(str(e))
+                
+    x = Counter(matchedAr)
+    print(x)
+    graphX = []
+    graphY = []
+
+    ylimi = 0
+
+    for eachThing in x:
+        graphX.append(eachThing)
+        graphY.append(x[eachThing])
+        ylimi = x[eachThing]
+
+
+
+    fig = plt.figure()
+    ax1 = plt.subplot2grid((4,4),(0,0), rowspan=1, colspan=4)
+    ax2 = plt.subplot2grid((4,4),(1,0), rowspan=3,colspan=4)
+    
+    ax1.imshow(iar)
+    ax2.bar(graphX,graphY,align='center')
+    plt.ylim(400)
+    
+    xloc = plt.MaxNLocator(12)
+    ax2.xaxis.set_major_locator(xloc)
+
+    plt.show()
+
+whatNumIsThis('images/test_3.png')
+
+
+# In[10]:
+
+i = Image.open('images/numbers/0.1.png')
+iar = np.array(i)
+i2 = Image.open('images/numbers/y0.4.png')
+iar2 = np.array(i2)
+i3 = Image.open('images/numbers/y0.5.png')
+iar3 = np.array(i3)
+i4 = Image.open('images/sentdex.png')
+iar4 = np.array(i4) 
+
+
+# In[11]:
+
+fig = plt.figure()
+ax1 = plt.subplot2grid((8,6),(0,0), rowspan=4, colspan=3)
+ax2 = plt.subplot2grid((8,6),(4,0), rowspan=4, colspan=3)
+ax3 = plt.subplot2grid((8,6),(0,3), rowspan=4, colspan=3)
+ax4 = plt.subplot2grid((8,6),(4,3), rowspan=4, colspan=3)
+
+ax1.imshow(iar)
+ax2.imshow(iar2)
+ax3.imshow(iar3)
+ax4.imshow(iar4)
+
+
+plt.show()
+
+
+# In[13]:
+
+i = Image.open('images/numbers/0.1.png')
+iar = np.array(i)
+i2 = Image.open('images/numbers/y0.4.png')
+iar2 = np.array(i2)
+i3 = Image.open('images/numbers/y0.5.png')
+iar3 = np.array(i3)
+i4 = Image.open('images/sentdex.png')
+iar4 = np.array(i4)
+
+
+iar = threshold(iar)
+iar2 = threshold(iar2)
+iar3 = threshold(iar3)
+iar4 = threshold(iar4)
+
+fig = plt.figure()
+ax1 = plt.subplot2grid((8,6),(0,0), rowspan=4, colspan=3)
+ax2 = plt.subplot2grid((8,6),(4,0), rowspan=4, colspan=3)
+ax3 = plt.subplot2grid((8,6),(0,3), rowspan=4, colspan=3)
+ax4 = plt.subplot2grid((8,6),(4,3), rowspan=4, colspan=3)
+
+ax1.imshow(iar)
+ax2.imshow(iar2)
+ax3.imshow(iar3)
+ax4.imshow(iar4)
+
+
+plt.show()
+
+
+# In[ ]:
+
+
+
